8b1039a5746f23ede29a0294aa30a5d44059202d
Lt.SaltySeaSlug <SaltySeaSlug@users.noreply.github.com>
2022-09-14 15:26:54 +0200

    Update index.js

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ md.use(emoji);
 
    /* import template pages */
 	const aboutMe = fs.readFileSync('./templates/about-me.md', 'utf8');
-	const badges =  generateBadges();
+	const badges =  promises.push(generateBadges());
 
   const headerImage = `<img src="https://i.imgur.com/RK1kR8g.png" alt="Mokkapps GitHub README header image">`;
   const twitterBadge = `[<img src="https://img.shields.io/badge/twitter-%231DA1F2.svg?&style=for-the-badge&logo=twitter&logoColor=white" height=${badgeHeight}>](${twitterUrl})`;
