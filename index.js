ed0570d802536087f29bc6f25cf024c627bb45a9
Lt.SaltySeaSlug <SaltySeaSlug@users.noreply.github.com>
2022-09-14 13:12:18 +0200

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const emoji = require("markdown-it-emoji");
 const fs = require("fs");
 const Parser = require("rss-parser");
 const parser = new Parser();
-
+const reader = new FileReader();
 
 
 
@@ -37,12 +37,13 @@ md.use(emoji);
   const instagramBadge = `[<img src="https://img.shields.io/badge/instagram-%23E4405F.svg?&style=for-the-badge&logo=instagram&logoColor=white" height=${badgeHeight}>](${instagramUrl})`;
   const mediumBadge = `[<img src="https://img.shields.io/badge/medium-%2312100E.svg?&style=for-the-badge&logo=medium&logoColor=white" height=${badgeHeight}>](${mediumUrl})`;
   const devToBadge = `[<img src="https://img.shields.io/badge/DEV.TO-%230A0A0A.svg?&style=for-the-badge&logo=dev-dot-to&logoColor=white" height=${badgeHeight}>](${devToUrl})`;
+  const aboutMe = reader.readAsDataURL('templates/about-me.md');
 
   const buyMeACoffeeButton = `<a href="https://www.buymeacoffee.com/mokkapps" target="_blank" rel="noreferrer nofollow">
       <img src="https://cdn.buymeacoffee.com/buttons/default-red.png" alt="Buy Me A Coffee" height="40" width="170" >
     </a>`;
 
-  const text = `${include templates\about-me.md}\n\n{headerImage}\n\n
+  const text = `${aboutMe}\n\n{headerImage}\n\n
   ${twitterBadge} ${linkedInBadge} ${instagramBadge} ${mediumBadge} ${devToBadge}\n\n
   [:arrow_right: Check out my website](${websiteUrl})\n\n
   ${buyMeACoffeeButton}\n\n
