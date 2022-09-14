3f3cc909dff72f2d7734971e49e93712b80f9c17
Lt.SaltySeaSlug <SaltySeaSlug@users.noreply.github.com>
2022-09-14 14:11:35 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const md = require("markdown-it")({
   html: true, // Enable HTML tags in source
   breaks: true, // Convert '\n' in paragraphs into <br>
   linkify: true, // Autoconvert URL-like text to links
-}).use("markdown-it-highlightjs");
+});
 
-
+const highlight = require("markdown-it-highlightjs");
 const emoji = require("markdown-it-emoji");
 const fs = require("fs");
 const Parser = require("rss-parser");
@@ -22,6 +22,7 @@ const blogPostLimit = 5;
 const badgeHeight = "25";
 
 md.use(emoji);
+md.use(highlight);
 
 (async () => {
   let blogPosts = "";
